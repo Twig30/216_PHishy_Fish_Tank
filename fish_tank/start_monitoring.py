afe6094c512168085292c3897d9e04c486482c93
Twig30 <immanuel.delgado@stu.vertexacademies.org>
2026-03-13 18:18:47 +0000

    debugged a small portion of code

---
+++ fish_tank/start_monitoring.py
@@ -0,0 +1,5 @@
+#Start_monititoring.py
+from fishtank import FishTankCLI #import the class
+app = FishTankCLI()              #create an instance of FishTankCLI
+app.main()                        #run the main function
+
